ee18a2c4394f951514254f20520bc4558f61c5e3
kwan_mohara <kwan@mohara.co>
2020-05-11 16:19:19 +0700

    from_shell_script

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import initStoryshots from '@storybook/addon-storyshots';
 import App from './App';
+initStoryshots();
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
---
+++ stories/2-App.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from '../src/App.js';
+
+export default {
+  title: 'App',
+  component: App,
+};
+
+export const HomePage = () => <App />;
+
